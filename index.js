1da340df12a00901e89aa291033c2c614c0356bf
Damian Michalski <damian-michalski@tlen.pl>
2019-01-29 14:14:33 +0100

    node http

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
         fs.readFile('./index.html', 'utf-8', function (err, data) {
+          if (err) throw err;
             response.write(data);
             console.log(data);
+          response.end();
         });
-        response.end();
+
     } else {
-        fs.readFile('./images/404.jpg', function (err, data) {
-            response.write('<img src="./images/404.jpg" alt="Wrong address!">');
-        });
+      response.setHeader("Content-Type", "image/jpg; charset=utf-8");
+      response.statusCode = 404;
+      fs.readFile('./images/404.png', function(err, data){
+        if (err) throw err;
+        response.write(data);
         response.end();
+      });
+
     }
 });
 
