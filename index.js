b2b41273f99726c56e08155a7f95b79d5666d862
DamianM <damian.michalski@kramp.com>
2019-01-30 07:59:07 +0100

    node http

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
-        fs.readFile('./index.html', 'utf-8', function (err, data) {
+        fs.readFile('./index.html', function (err, data) {
           if (err) throw err;
-            response.write(data);
-            console.log(data);
+          response.write(data);
           response.end();
         });
 
     } else {
       response.setHeader("Content-Type", "image/jpg; charset=utf-8");
       response.statusCode = 404;
-      fs.readFile('./images/404.png', function(err, data){
+      fs.readFile('./404.png', function(err, data){
         if (err) throw err;
         response.write(data);
         response.end();
